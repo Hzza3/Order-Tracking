d7b36ff3d501ecb926b167e2d0321690854bee60
A.Hazzaa <ahazzaa@arabdt.com>
2024-04-30 12:32:42 +0300

    Initial Commit

---
+++ Order Tracking/Order_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_TrackingApp.swift
+//  Order Tracking
+//
+//  Created by Ahmed Hazzaa on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Order_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
